8a137c4784c7177799777e40443f7cd4735b59f3
Jinlee <jinlee925@gmail.com>
2021-07-22 18:58:43 +0900

    기본코드 작성

---
+++ bizNo.py
@@ -0,0 +1,27 @@
+import requests
+import json
+
+def getbiz(number):
+    headers = {
+        'accept': 'application/json',
+        'Authorization': '2tNRx2X2iaJxyI8f7b1NDfI1zaNobxIOF7kVm4aW52G7xMLw6T4trCLd7yc10gzPHuIYXvQXgnldjcdpu30LcA==',
+        'Content-Type': 'application/json',
+    }
+
+    params = (
+        ('serviceKey', '2tNRx2X2iaJxyI8f7b1NDfI1zaNobxIOF7kVm4aW52G7xMLw6T4trCLd7yc10gzPHuIYXvQXgnldjcdpu30LcA=='),
+    )
+
+    data = '{ "b_no": [ "' + str(number) + '" ]}'
+
+
+    response = requests.post('https://api.odcloud.kr/api/nts-businessman/v1/status', headers=headers, params=params,
+                             data=data)
+    res = json.loads(response.text)
+    data = res['data']
+    if data[0]['tax_type'] == "국세청에 등록되지 않은 사업자등록번호입니다.":
+        return data[0]['tax_type']
+    else:
+        return "사업자등록번호 '" + data[0]['b_no'] + "'은 " + data[0]['tax_type'] + "입니다."
+
+print(getbiz(1048186269))
---
+++ housingPrice.py
@@ -0,0 +1,25 @@
+from urllib.request import Request, urlopen
+from urllib.parse import urlencode, quote_plus
+import xmltodict
+import json
+
+
+def HousingPrice(pnu, stdrYear):
+    url = 'http://apis.data.go.kr/1611000/nsdi/IndvdHousingPriceService/attr/getIndvdHousingPriceAttr'
+    serviceKey = '2tNRx2X2iaJxyI8f7b1NDfI1zaNobxIOF7kVm4aW52G7xMLw6T4trCLd7yc10gzPHuIYXvQXgnldjcdpu30LcA=='
+
+    queryParams = '?' + urlencode({
+        quote_plus('serviceKey'): serviceKey,
+        quote_plus('pnu'): str(pnu),
+        quote_plus('stdrYear'): str(stdrYear),
+    })
+
+    request = Request(url + queryParams)
+    request.get_method = lambda: 'GET'
+    response_body = urlopen(request).read()
+    jsonString = json.loads(json.dumps(xmltodict.parse(response_body), ensure_ascii=False))
+    result = jsonString['response']['fields']['field']
+    return result
+
+
+print(HousingPrice(1111016700100200000, 2020))
